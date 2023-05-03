e9e6ea9b138dccd45076995fbe15f48dd6986893
ashley2173822 <ashley.rodas@qaill.edu.gt>
2023-05-03 12:30:31 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//Crear una interfaz
+interface Tarjeta{
+  titulo: string;
+  subtitulo:string;
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProyectoAngular';
+export class AppComponent implements OnInit {
+  title = 'Mi primer app en Angular';
+  public ArregloTarjeta: Tarjeta []=[];
+
+ngOnInit():void {//inicializar cuando se ejecuta la pagina}
+ this.ArregloTarjeta= [
+{titulo:'Video 1', subtitulo: 'Subtitulo de Video',image:"./assets/images/img7.jpg"},
+{titulo:'Video 2', subtitulo: 'Subtitulo de Video',image:"./assets/images/img2.jpg"},
+{titulo:'Video 3', subtitulo: 'Subtitulo de Video',image:"./assets/images/img3.jpg"},
+{titulo:'Video 4', subtitulo: 'Subtitulo de Video',image:"./assets/images/img5.jpg"},
+{titulo:'Video 5', subtitulo: 'Subtitulo de Video',image:"./assets/images/img6.jpeg"},
+
+
+
+ ]
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    CardComponent,
+  
   ],
   imports: [
     BrowserModule
